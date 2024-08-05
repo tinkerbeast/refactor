f89104379e5b1129e9426031c783cbb432119b19
Rishin Goswami <rishin.goswami@gmail.com>
2024-08-05 19:02:09 +0530

    Feature using xml for selection

--- refactor.py
+++ refactor.py
@@ -1,17 +1,19 @@
 import ast
 import re
 
+from lxml.builder import E
+
+
 def load(src: str):
     r = Refactor()
     r.load(src)
     return r
 
+
 class Refactor:
 
     def __init__(self):
-        self.filters_fn = []
-        self.filters_param = []
-        self.filter_idx = 0
+        self.ast_idx = 0
         self.changes = []
 
     def load(self, src:str):
@@ -32,6 +34,8 @@ class Refactor:
             prev_len += 1 + len(lines[i])
         self.line_map  = o
         self.line_cnt = len(lines)
+        # Walk the AST
+        self.xml = self.walk_ast_bottom_up_(self.ast, self.lxml_builder_fn_)
 
     def text(self, node:ast.AST) -> str:
         if not hasattr(node, 'lineno'):
@@ -48,7 +52,7 @@ class Refactor:
         out = re.sub(pattern, repl, self.text(node), count, flags)
         self.changes.append((b, e, out))
 
-    def filter_(self, node: ast.AST, filter_type, filter_fn):
+    def lxml_builder_fn_(self, node: ast.AST|str, children):
         # Filter out nodes which can't be converted to text.
         if type(node) in (ast.Add, ast.And, ast.arguments, ast.BitAnd,
                 ast.BitOr, ast.BitXor, ast.comprehension, ast.Del, ast.Div,
@@ -57,39 +61,65 @@ class Refactor:
                 ast.match_case, ast.Mod, ast.Mult, ast.Not, ast.NotEq,
                 ast.NotIn, ast.Or, ast.Pow, ast.RShift, ast.Store, ast.Sub,
                 ast.UAdd, ast.USub, ast.withitem):
-            return
-        #
+            return None
+        # Remove None from children
+        children = [c for c in children if c is not None]
+        if isinstance(node, str):
+            return E('_' + node, *children)
         else:
-            raise ValueError('Filter type not supported yet')
+            #print(node._attributes) # TODO: decide on line numbers
+            ntype = type(node).__name__
+            self.ast_idx += 1
+            attrs = {'idx_': str(self.ast_idx)}
+            for f in node._fields:
+                sub_node = getattr(node, f)
+                if isinstance(sub_node, ast.AST) or isinstance(sub_node, list):
+                    pass
+                else:
+                    attrs[f] = str(sub_node)
+            # For the xml node
+            #print(ntype)
+            return E(ntype, *children, **attrs)
 
-    def filt(self, node_type, fn): # TODO(rishin): Return type must be str
-        self.filters_type.append(node_type)
-        self.filters_fn.append(fn)
-        return self
+    def print_fn_(self, node, parent, level):
+        if type(node) in (ast.Add, ast.And, ast.arguments, ast.BitAnd,
+                ast.BitOr, ast.BitXor, ast.comprehension, ast.Del, ast.Div,
+                ast.Eq, ast.FloorDiv, ast.Gt, ast.GtE, ast.In, ast.Invert,
+                ast.Is, ast.IsNot, ast.Load, ast.LShift, ast.Lt, ast.LtE,
+                ast.match_case, ast.Mod, ast.Mult, ast.Not, ast.NotEq,
+                ast.NotIn, ast.Or, ast.Pow, ast.RShift, ast.Store, ast.Sub,
+                ast.UAdd, ast.USub, ast.withitem):
+            pass
+        else:
+            print('{}{}'.format('  ' * level, type(node).__name__))
 
-    def walk_ast_(self, node, fn, parent, level):
+    def walk_ast_top_down_(self, node, fn, parent, level):
         if isinstance(node, ast.AST):
             if parent is not None:
                 fn(node, parent, level)
             for name in node._fields:
                 value = getattr(node, name, None)
                 if value is not None:
-                    self.walk_ast_(value, fn, node, level + 1)
+                    self.walk_ast_top_down_(value, fn, node, level + 1)
         elif isinstance(node, list):
             for n in node:
-                self.walk_ast_(n, fn, parent, level + 1)
+                self.walk_ast_top_down_(n, fn, parent, level + 1)
         else:
             pass # terminal string values of tokens
 
-    def process_(self, node:ast.AST, parent:ast.AST, level:int):
-        filter_type = self.filters_type[self.filter_idx]
-        filter_fn = self.filters_type[self.filter_idx]
-        self.filter_(filters_type, filter_fn)
+    def walk_ast_bottom_up_(self, node, fn):
+        if not isinstance(node, ast.AST):
+            raise ValueError('AST walking error')
+        children = {name: getattr(node, name, None) for name in node._fields}
+        fn_children = []
+        for name, c in children.items():
+            if isinstance(c, list):
+                fn_sub_children = [self.walk_ast_bottom_up_(i, fn) for i in c]
+                fn_children.append(fn(name, fn_sub_children))
+            elif isinstance(c, ast.AST):
+                fn_children.append(self.walk_ast_bottom_up_(c, fn))
+            else:
+                pass # Primitive attributes.
+        return fn(node, fn_children)
 
-    def process():
-        filters_cnt = len(self.filters_type)
-        while self.filter_idx < filters_cnt:
-            self.walk_ast_(self.ast, self.process_, None, 0)
-            new_data = self.collate_changes_()
-            self.loads(new_data)
-            self.filter_idx += 1
+
