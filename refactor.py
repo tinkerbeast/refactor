9a372f6d80ba5c163844fd6b544921a4f1c6501b
Rishin Goswami <rishin.goswami@gmail.com>
2024-08-29 23:06:01 +0530

    Refactor to remove dead code and do style fixes

--- refactor.py
+++ refactor.py
@@ -19,13 +19,13 @@ class Refactor:
         self.changes = []
         self.xml_filter = []
 
-    def load(self, src:str):
+    def load(self, src: str):
         # Read and parse source file.
         with open(src, 'r') as fd:
             data = fd.read()
         return self.loads(data)
 
-    def loads(self, data:str):
+    def loads(self, data: str):
         self.data = data
         self.ast = ast.parse(self.data, mode='exec')
         # Map line numbers to byte index.
@@ -35,46 +35,47 @@ class Refactor:
         for i in range(len(lines)):
             o[i] = prev_len
             prev_len += 1 + len(lines[i])
-        self.line_map  = o
+        self.line_map = o
         self.line_cnt = len(lines)
         # Walk the AST
         self.xml = self.walk_ast_bottom_up_(self.ast, self.lxml_builder_fn_)
-        #print(ast.dump(self.ast, indent=2))
 
-    def text(self, node:ast.AST) -> str:
+    def text(self, node: ast.AST) -> str:
         if not hasattr(node, 'lineno'):
             raise ValueError('Node cannot be converted to text')
         b = self.line_map[node.lineno - 1] + node.col_offset
         e = self.line_map[node.end_lineno - 1] + node.end_col_offset
         return self.data[b:e]
 
-    def sub_(self, pattern, repl, node:ast.AST, count=0, flags=0) -> None:
+    def sub_(self, pattern, repl, node: ast.AST, count=0, flags=0) -> None:
         if not hasattr(node, 'lineno'):
             raise ValueError('Node cannot be converted to text')
         b = self.line_map[node.lineno - 1] + node.col_offset
         e = self.line_map[node.end_lineno - 1] + node.end_col_offset
-        #print((pattern, repl, self.text(node)))
+        # print((pattern, repl, self.text(node)))
         out = re.sub(pattern, repl, self.text(node), count, flags)
-        #print((b, e, out))
+        # print((b, e, out))
         self.changes.append((b, e, out))
 
-    def lxml_builder_fn_(self, node: ast.AST|str, children):
-        #print(node, type(node))
+    def lxml_builder_fn_(self, node: ast.AST | str, children):
+        # print(node, type(node))
         # Filter out nodes which can't be converted to text.
-        if type(node) in (ast.Add, ast.And, ast.BitAnd,
-                ast.BitOr, ast.BitXor, ast.comprehension, ast.Del, ast.Div,
-                ast.Eq, ast.FloorDiv, ast.Gt, ast.GtE, ast.In, ast.Invert,
-                ast.Is, ast.IsNot, ast.Load, ast.LShift, ast.Lt, ast.LtE,
-                ast.match_case, ast.Mod, ast.Mult, ast.Not, ast.NotEq,
-                ast.NotIn, ast.Or, ast.Pow, ast.RShift, ast.Store, ast.Sub,
-                ast.UAdd, ast.USub, ast.withitem):
+        # TODO(rishigos): Group AST token into similar groups
+        if type(node) in (
+            ast.Add, ast.And, ast.BitAnd, ast.BitOr, ast.BitXor,
+            ast.comprehension, ast.match_case, ast.withitem,
+            ast.Del, ast.Div, ast.Eq, ast.FloorDiv, ast.Gt, ast.GtE, ast.In,
+            ast.Invert, ast.Is, ast.IsNot, ast.Load, ast.LShift, ast.Lt, ast.LtE,
+            ast.Mod, ast.Mult, ast.Not, ast.NotEq, ast.NotIn, ast.Or,
+            ast.Pow, ast.RShift, ast.Store, ast.Sub, ast.UAdd, ast.USub,
+        ):
             return None
         # Remove None from children
         children = [c for c in children if c is not None]
         if isinstance(node, str):
             return E('_' + node, *children)
         elif isinstance(node, ast.AST):
-            #print(node._attributes) # TODO: decide on line numbers
+            # print(node._attributes) # TODO: decide on line numbers
             self.ast_idx += 1
             self.ast_map[self.ast_idx] = node
             attrs = {'idx_': str(self.ast_idx)}
@@ -95,18 +96,20 @@ class Refactor:
             raise ValueError('Invalid parser implementation')
 
     def walk_ast_top_down_(self, node, fn, parent, level):
-        if isinstance(node, ast.AST):
-            if parent is not None:
-                fn(node, parent, level)
-            for name in node._fields:
-                value = getattr(node, name, None)
-                if value is not None:
-                    self.walk_ast_top_down_(value, fn, node, level + 1)
-        elif isinstance(node, list):
-            for n in node:
-                self.walk_ast_top_down_(n, fn, parent, level + 1)
-        else:
-            pass # terminal string values of tokens
+        # TODO(rishigos): Remove this - Kept for historic reasons
+        # if isinstance(node, ast.AST):
+        #    if parent is not None:
+        #        fn(node, parent, level)
+        #    for name in node._fields:
+        #        value = getattr(node, name, None)
+        #        if value is not None:
+        #            self.walk_ast_top_down_(value, fn, node, level + 1)
+        # elif isinstance(node, list):
+        #    for n in node:
+        #        self.walk_ast_top_down_(n, fn, parent, level + 1)
+        # else:
+        #    pass # terminal string values of tokens
+        pass
 
     def walk_ast_bottom_up_(self, node, fn):
         if not isinstance(node, ast.AST):
@@ -120,34 +123,8 @@ class Refactor:
             elif isinstance(c, ast.AST):
                 fn_children.append(self.walk_ast_bottom_up_(c, fn))
             else:
-                pass # Primitive attributes.
+                pass  # Primitive attributes.
         return fn(node, fn_children)
-
-    def filter_node(self, node_type, attr=None, pattern=None):
-        new_flat_ast = []
-        for i in self.flat_ast:
-            if type(i[0]) != node_type:
-                continue
-            if attr is not None and not hasattr(i[0], attr):
-                continue
-            item = i[0] if attr is None else getattr(i[0], attr)
-            if pattern is None:
-                new_flat_ast.append(i)
-                continue
-            #print(f'Debug: filter_node@re.match {pattern} {self.text(item)}')
-            if re.match(pattern, self.text(item)):
-                new_flat_ast.append(i)
-                continue
-        self.flat_ast = new_flat_ast
-        return self
-
-    def select_attr(self, attr):
-        new_flat_ast = []
-        for i in self.flat_ast:
-            if hasattr(i[0], attr):
-                new_flat_ast.append([getattr(i[0], attr)])
-        self.flat_ast = new_flat_ast
-        return self
 
     def collate_changes_(self):
         # Check for overlaps
@@ -157,12 +134,13 @@ class Refactor:
             overlaps[e] -= 1
         for i in range(1, len(overlaps)):
             overlaps[i] += overlaps[i - 1]
-            if overlaps[i] > 1: raise ValueError('Overlapping changes cannot be processed')
+            if overlaps[i] > 1:
+                raise ValueError('Overlapping changes cannot be processed')
         sorted_changes = sorted(self.changes, key=lambda x: x[0])
         last_e = 0
         prev = ''
         for b, e, out in sorted_changes:
-            #print(f'{prev}><{out}')
+            # print(f'{prev}><{out}')
             prev += self.data[last_e:b]
             prev += out
             last_e = e
@@ -189,7 +167,6 @@ class Refactor:
             self.changes.append((b, e, out))
         new_data = self.collate_changes_()
         return new_data
-
 
     def xpath(self, path):
         xout = []
@@ -222,5 +199,3 @@ class Refactor:
             else:
                 print(f'>{idx}: {i}: ???<')
         return self
-
-
--- utils.py
+++ utils.py
@@ -8,14 +8,13 @@ import uuid
 
 def decorate_fns(src, decorator):
     # Function to return annotated body
-    def indented_replace(txt, node:ast.AST):
+    def indented_replace(txt, node: ast.AST):
         p = '^.*$'
         s = f'{decorator}\n' + ' ' * node.col_offset + '\\g<0>'
         return re.sub(p, s, txt, flags=re.DOTALL)
+
     # Search and replace
-    out = refactor.load(src) \
-        .xpath('//FunctionDef') \
-        .map_fn(indented_replace)
+    out = refactor.load(src).xpath('//FunctionDef').map_fn(indented_replace)
     #
     return out
 
@@ -25,7 +24,8 @@ class ArgReplacer:
         self.arg_idx = 0
         self.args = args
         self.kwargs = kwargs
-    def __call__(self, txt, node:ast.AST):
+
+    def __call__(self, txt, node: ast.AST):
         p = '^.*$'
         if self.arg_idx < len(self.args):
             s = '\\g<0>: {}'.format(self.args[self.arg_idx])
@@ -35,19 +35,17 @@ class ArgReplacer:
             s = '\\g<0>: {}'.format(self.kwargs[node.arg])
             return re.sub(p, s, txt)
 
+
 def annotate_fn_params(src, func_name, args, kwargs, ret):
     # Add return type
-    out = refactor.load(src) \
-        .xpath(f'//FunctionDef[@name="{func_name}"]') \
+    out = (
+        refactor.load(src)
+        .xpath(f'//FunctionDef[@name="{func_name}"]')
         .map_str(r'^(.+?):(.+)$', r'\1 -> {}:\2'.format(ret), flags=re.DOTALL)
+    )
     # Add type to args
     r = refactor.Refactor()
     r.loads(out)
-    out = r.xpath(f'//FunctionDef[@name="{func_name}"]//arg') \
-        .map_fn(ArgReplacer(args, kwargs))
+    out = r.xpath(f'//FunctionDef[@name="{func_name}"]//arg').map_fn(ArgReplacer(args, kwargs))
     #
     return out
-
-
-
-
